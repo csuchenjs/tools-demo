c3f671d00d2d6ebbc430dae4321536d90cf22393
csuchenjs <csuchenjs@gmail.com>
2018-07-22 15:10:24 +0800

    ping domain in file

---
+++ kubernetes-demo/pingfile/pingfile.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/tatsushid/go-fastping"
+)
+
+func getIPAddr(p *fastping.Pinger, hostname string) string {
+	var address string
+	ra, err := net.ResolveIPAddr("ip4:icmp", hostname)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	p.AddIPAddr(ra)
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		address = addr.String()
+		//fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+	}
+	p.OnIdle = func() {
+		//fmt.Println("finish")
+	}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+	p.RemoveIPAddr(ra)
+	return address
+}
+
+func readFile(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	defer file.Close()
+
+	var records []string
+	reader := bufio.NewReader(file)
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		records = append(records, string(line))
+	}
+	return records
+}
+
+func pingIPFromFile(filename string) {
+	records := readFile(filename)
+	if len(records) == 0 {
+		fmt.Println("no records in file")
+		return
+	}
+
+	tmpFilename := filename + ".ping"
+	file, err := os.OpenFile(tmpFilename, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	p := fastping.NewPinger()
+	writer := bufio.NewWriter(file)
+	for _, r := range records {
+		if strings.HasPrefix(r, "//") {
+			writer.WriteString(r + "\r\n")
+			continue
+		}
+
+		items := strings.Split(r, "\t")
+		if len(items) <= 1 {
+			writer.WriteString(r + "\r\n")
+			continue
+		}
+
+		fmt.Printf("Resolving %v...\t", items[0])
+		addr := getIPAddr(p, items[0])
+		fmt.Printf("ping addr: %v\n", addr)
+		writer.WriteString(items[0] + "\t" + addr + "\t" + items[len(items)-1] + "\r\n")
+	}
+
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// move file
+	file.Close()
+	err = os.Rename(tmpFilename, filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// copy filename+ping to filename
+	// delete filename+ping
+}
+
+func main() {
+	if os.Args[1] == "-f" {
+		pingIPFromFile(os.Args[2])
+	} else if os.Args[1] == "-s" {
+		p := fastping.NewPinger()
+		addr := getIPAddr(p, os.Args[2])
+		fmt.Printf("IP Addr: %s receive\n", addr)
+	} else {
+		fmt.Println("invalid args")
+		os.Exit(-1)
+	}
+}
